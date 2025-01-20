7ded768f04166275f058dd2969f1d254a2cc410a
Isabela <isabela.iar01@gmail.com>
2025-01-20 15:16:49 +1000

    chore(main): integrate updated components and routes

--- cybersharks-front-end/src/main.jsx
+++ cybersharks-front-end/src/main.jsx
@@ -7,26 +7,40 @@ import "./styles/globals.css";
 
 // Here we import out pages
 import HomePage from "./pages/HomePage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 // Here we import our components
 import NavBar from "./components/NavBar.jsx";
-import SignupPage from "./pages/SignupPage.jsx";
+import Footer from "./components/Footer.jsx";
 
-// Here we create our router and tell it whats pages to render at what path
+// Here we create our router and tell it what pages to render at what path
 const router = createBrowserRouter([
-  // These are the three routes!
   {
     path: "/",
-    // Putting our NavBar as the main component will causes the children to render in the <Outlet />
-    // element: <NavBar />,
-    //     children: [{ path: "/", element: <HomePage /> }],
-    //     children: [{ path: "/signup", element: <SignupPage /> }],
-    //   },
-    // ]);
-    element: <HomePage />, // HomePage renders Header, including NavBar
+    element: (
+      <>
+        <NavBar />
+        <HomePage />
+        <Footer />
+      </>
+    ),
     children: [{ path: "/signup", element: <SignupPage /> }],
   },
 ]);
+// const router = createBrowserRouter([
+//   // These are the three routes!
+//   {
+//     path: "/",
+//     // Putting our NavBar as the main component will causes the children to render in the <Outlet />
+//     // element: <NavBar />,
+//     //     children: [{ path: "/", element: <HomePage /> }],
+//     //     children: [{ path: "/signup", element: <SignupPage /> }],
+//     //   },
+//     // ]);
+//     element: <HomePage />, // HomePage renders Header, including NavBar
+//     children: [{ path: "/signup", element: <SignupPage /> }],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
