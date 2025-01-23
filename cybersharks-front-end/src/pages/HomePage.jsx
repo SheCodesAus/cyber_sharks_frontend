e9b8803ebddbf129421e164e73c4c9536528761c
Isabela <isabela.iar01@gmail.com>
2025-01-23 17:34:20 +1000

    revamp homepage layout and component integration

--- cybersharks-front-end/src/pages/HomePage.jsx
+++ cybersharks-front-end/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import IntroSection from "../components/homepage/IntroSection";
 import StatsSection from "../components/homepage/StatsSection";
 import FeaturedSpeakers from "../components/homepage/FeaturedSpeakers";
 import MissionStatement from "../components/homepage/MissionStatement";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
@@ -10,11 +11,11 @@ function HomePage() {
       <NavBar />
       <div className="pt-16">
         {" "}
-        {/* Add padding to prevent overlap with the fixed NavBar */}
         <IntroSection />
         <StatsSection />
         <FeaturedSpeakers />
         <MissionStatement />
+        <Footer />
       </div>
     </div>
   );
