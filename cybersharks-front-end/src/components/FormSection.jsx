ca0ca2c87be18954ddf9ec42700ad7fa6ef4c8b2
yesol <yeahsol.kwon@gmail.com>
2025-01-26 08:28:15 +1000

    fix form layout

--- cybersharks-front-end/src/components/FormSection.jsx
+++ cybersharks-front-end/src/components/FormSection.jsx
@@ -1,9 +1,9 @@
 function FormSection({ title, description, children }) {
   return (
-    <div className="flex justify-between">
-      <div className="flex flex-col w-[50%]">
-        <h1>{title}</h1>
-        <p>{description}</p>
+    <div className="grid grid-cols-2 border-b-[1px] border-accent1  max-h-80  py-6">
+      <div className="flex flex-col flex-1 gap-3 ">
+        <h1 className="font-bold">{title}</h1>
+        <p className="max-w-[90%]">{description}</p>
       </div>
       {children}
     </div>
--- cybersharks-front-end/src/pages/CreatePortfolio.jsx
+++ cybersharks-front-end/src/pages/CreatePortfolio.jsx
@@ -2,80 +2,116 @@ import FormSection from "../components/FormSection";
 
 function CreatePortfolio() {
   return (
-    <form className="flex flex-col gap-4 pt-20">
-      {/* Name */}
-      <FormSection
-        title="Name"
-        description="This is the name that will appear on your public profile. Use your real name or a professional alias that represents you best."
-      >
-        <input className="bg-red-300 w-[40%]" />
-      </FormSection>
-
-      {/* Biography */}
-      <FormSection
-        title="Biography"
-        description="Introduce yourself with a brief biography. Include your professional background, key achievements, and passions."
-      >
-        <textarea className="bg-red-300 w-[40%]" />
-      </FormSection>
-
-      {/* Experience Level */}
-      <FormSection
-        title="Experience Level"
-        description="Choose your professional experience level to give visitors an idea of your expertise."
-      >
-        <input className="bg-red-300 w-[40%]" />
-      </FormSection>
-
-      {/* Photo */}
-      <FormSection
-        title="Photo"
-        description="Upload a profile picture using a publicly accessible image URL. Recommended dimensions: 400x400 pixels."
-      >
-        <input className="bg-red-300 w-[40%]" />
-      </FormSection>
-
-      {/* Contact */}
-      <FormSection
-        title="Contact"
-        description="Provide your contact details so visitors can connect with you directly. Include an email address and a LinkedIn profile."
-      >
-        <div className="flex flex-col gap-2">
-          <div>
-            <h1>Email</h1>
-            <input className="bg-red-300" />
+    <div className="flex flex-col justify-center items-center min-h-screen bg-customWhite">
+      <h1 className="pt-20">Start your portfolio </h1>
+      <form className="flex flex-col px-4 py-9 justify-center items-center bg-white shadow-md rounded-lg w-full max-w-7xl">
+        {/* Name */}
+        <FormSection
+          title="Name"
+          description="This is the name that will appear on your public profile. Use your real name or a professional alias that represents you best."
+        >
+          <div className="flex flex-col gap-2">
+            <label className="input input-bordered flex items-center gap-2 bg-white w-full">
+              First Name
+              <input
+                type="text"
+                className="grow"
+                name="firstName"
+                id="firstName"
+                required
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 bg-white ">
+              Last Name
+              <input
+                type="text"
+                className="grow"
+                name="lastName"
+                id="lastName"
+                required
+              />
+            </label>
           </div>
-          <div>
-            <h1>LinkedIn</h1>
-            <input className="bg-red-300" />
+        </FormSection>
+
+        {/* Biography */}
+        <FormSection
+          title="Biography"
+          description="Introduce yourself with a brief biography. Include your professional background, key achievements, and passions."
+        >
+          <textarea className="bg-red-300 w-full min-h-64" />
+        </FormSection>
+
+        {/* Experience Level */}
+        <FormSection
+          title="Experience Level"
+          description="Choose your professional experience level to give visitors an idea of your expertise."
+        >
+          <input className="w-full border-bl" />
+        </FormSection>
+
+        {/* Photo */}
+        <FormSection
+          title="Photo"
+          description="Upload a profile picture using a publicly accessible image URL. Recommended dimensions: 400x400 pixels."
+        >
+          <input className="bg-red-300 w-full" />
+        </FormSection>
+
+        {/* Contact */}
+        <FormSection
+          title="Contact"
+          description="Provide your contact details so visitors can connect with you directly. Include an email address and a LinkedIn profile."
+        >
+          <div className="flex flex-col gap-2">
+            <label className="input input-bordered flex items-center gap-2 bg-white w-full">
+              LinkedIn
+              <input
+                type="text"
+                className="grow"
+                name="firstName"
+                id="firstName"
+                required
+              />
+            </label>
+            <label className="input input-bordered flex items-center gap-2 bg-white ">
+              Email
+              <input
+                type="text"
+                className="grow"
+                name="lastName"
+                id="lastName"
+                required
+              />
+            </label>
           </div>
-        </div>
-      </FormSection>
+        </FormSection>
 
-      {/* Location */}
-      <FormSection
-        title="Location"
-        description="Specify your current city and country. This helps potential collaborators or employers know where you are based."
-      >
-        <input className="bg-red-300 w-[40%]" />
-      </FormSection>
+        {/* Location */}
+        <FormSection
+          title="Location"
+          description="Specify your current city and country. This helps potential collaborators or employers know where you are based."
+        >
+          <input className="bg-red-300 w-full" />
+        </FormSection>
 
-      {/* Topic */}
-      <FormSection
-        title="Topic"
-        description="Topics represent your interests, focus areas, or the key subjects you are passionate about."
-      >
-        <input className="bg-red-300 w-[40%]" />
-      </FormSection>
+        {/* Topic */}
+        <FormSection
+          title="Topic"
+          description="Topics represent your interests, focus areas, or the key subjects you are passionate about."
+        >
+          <input className="bg-red-300 w-full" />
+        </FormSection>
 
-      {/* Specialisation */}
-      <FormSection
-        title="Specialisation"
-        description="Highlight your key skills or areas of expertise. Use specific technologies or domains."
-      >
-        <input className="bg-red-300 w-[40%]" />
-      </FormSection>
-    </form>
+        {/* Specialisation */}
+        <FormSection
+          title="Specialisation"
+          description="Highlight your key skills or areas of expertise. Use specific technologies or domains."
+        >
+          <input className="bg-red-300 w-full" />
+        </FormSection>
+      </form>
+    </div>
   );
 }
 
