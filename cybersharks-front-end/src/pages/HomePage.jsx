8ad1f4aaa39d30b91cb71ed5ce509784c2dde9ef
Isabela <isabela.iar01@gmail.com>
2025-01-25 10:53:27 +1000

    feat: refactored HomePage to integrate updated homepage sections

--- cybersharks-front-end/src/pages/HomePage.jsx
+++ cybersharks-front-end/src/pages/HomePage.jsx
@@ -5,18 +5,36 @@ import FeaturedSpeakers from "../components/homepage/FeaturedSpeakers";
 import MissionStatement from "../components/homepage/MissionStatement";
 import Footer from "../components/Footer";
 
+// function HomePage() {
+//   return (
+//     <div className="min-h-screen bg-customWhite text-customBlack">
+//       <NavBar />
+//       <div className="pt-16">
+//         {" "}
+//         <IntroSection />
+//         <StatsSection />
+//         <FeaturedSpeakers />
+//         <MissionStatement />
+//         <Footer />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default HomePage;
+
 function HomePage() {
   return (
     <div className="min-h-screen bg-customWhite text-customBlack">
       <NavBar />
-      <div className="pt-16">
-        {" "}
+      <main className="pt-16">
         <IntroSection />
-        <StatsSection />
+
         <FeaturedSpeakers />
+        <StatsSection />
         <MissionStatement />
-        <Footer />
-      </div>
+      </main>
+      <Footer />
     </div>
   );
 }
