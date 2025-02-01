b3d9ad80e91fdb322007161d12304396830cc41f
yesol <yeahsol.kwon@gmail.com>
2025-02-01 17:18:50 +1000

    homepage button css fix

--- cybersharks-front-end/src/components/homepage/MissionStatement.jsx
+++ cybersharks-front-end/src/components/homepage/MissionStatement.jsx
@@ -7,7 +7,7 @@ function MissionStatement() {
       className="bg-[#1a1a1a] text-[#FFFEFD] py-20 pb-32"
     >
       <div className="max-w-7xl mx-auto px-6">
-        <div className="md:w-2/3">
+        <div className="md:w-[80%]">
           <h2 className="text-4xl font-serif font-bold leading-tight mb-4">
             That's{" "}
             <span className="relative">
@@ -28,13 +28,13 @@ function MissionStatement() {
               </svg>
             </span>
           </h2>
-          <div className="flex justify-between items-start gap-8">
+          <div className="flex flex-col sm:flex-row justify-between items-start gap-11">
             <p className="text-lg leading-relaxed max-w-xl">
               Our mission is to increase the diverse voices and perspectives.
               That's why we work to connect event organizers with inspiring
               speakers who share your path of making your journey unforgettable.
             </p>
-            <button className="bg-white text-[#1a1a1a] font-medium rounded-full px-6 py-3 hover:bg-[#FF6602] transition-all -ml-24">
+            <button className="bg-white text-[#1a1a1a] font-medium rounded-full px-6 py-3 hover:bg-[#FF6602] transition-all ">
               Find Speakers
             </button>
           </div>
