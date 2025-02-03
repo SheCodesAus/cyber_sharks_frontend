5ab29f7c11293a68e88cfff645991034e236753c
Isabela <isabela.iar01@gmail.com>
2025-02-03 11:23:59 +1000

    URL updated

--- cybersharks-front-end/src/components/StatsCard.jsx
+++ cybersharks-front-end/src/components/StatsCard.jsx
@@ -1,16 +1,3 @@
-// const StatsCard = ({ percentage, description }) => {
-//   return (
-//     <div className="text-left">
-//       <div className="flex items-baseline gap-1">
-//         <span className="text-6xl font-bold text-[#FFFEFD]">{percentage}</span>
-//         <span className="text-3xl font-bold text-[#FF6602]">%</span>
-//       </div>
-//       <p className="text-lg mt-4">{description}</p>
-//     </div>
-//   );
-// };
-
-// export default StatsCard;
 const StatsCard = ({ percentage, description, symbol }) => {
   return (
     <div className="text-left">
